89758634724ad1e3ebb150e58018d844a4d8c2b2
Kaijie Zheng <kaijiejz50@gmail.com>
2015-10-19 12:57:32 -0700

    Implement all methods to pass specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,58 @@
-$.ajax({
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'GET',
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chaatterbox: Messages received');
-  },
-  error: function (data) {
-    console.error('chatterbox: Failed to retrieve messages');
-  }
-});
+var app = {
+};
+
+app.init = function() {
+  this.server = 'https://api.parse.com/1/classes/chatterbox';
+  $('body').on('click', '.username', this.addFriend());
+  $('body').on('click', '#send .submit', this.handleSubmit());
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function () {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Messages received');
+      console.log(data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to retrieve messages');
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.addMessage = function(message) {
+  $('#chats').append('<p class="' + message.username + '">' + message.text + '</p>');
+};
+
+app.addRoom = function(message) {
+  $('#roomSelect').append('<p>' + message.room + '</p>');
+};
+
+app.addFriend = function() {
+
+};
+
+app.handleSubmit = function() {
+
+};
