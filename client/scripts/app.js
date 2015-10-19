e856b982994c7413d0c2db9a5cfe5781135ead49
Kaijie Zheng <kaijiejz50@gmail.com>
2015-10-19 11:34:25 -0700

    Add simple ajax GET request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,11 @@
-// YOUR CODE HERE:
+$.ajax({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chaatterbox: Messages received');
+  },
+  error: function (data) {
+    console.error('chatterbox: Failed to retrieve messages');
+  }
+});
